0411382d6f64df3a24112f2c17a83d94a964153d
David <thedrdvd@gmail.com>
2020-11-24 18:42:08 +0100

    Added the 'api' layer.

---
+++ parlem-back/api/__init__.py
@@ -0,0 +1,1 @@
+from . import brand, customer, product
---
+++ parlem-back/api/brand.py
@@ -0,0 +1,17 @@
+from business import BrandBusiness
+from flask import Blueprint, jsonify, request
+
+blueprint = Blueprint('brand', __name__)
+business = BrandBusiness()
+
+@blueprint.route('/api/brand/all', methods=['GET'])
+def api_get_all_brands():
+    return jsonify(business.get_all_brands(True))
+
+@blueprint.route('/api/brand', methods=['GET'])
+def api_get_brand_by_id():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+        return jsonify(business.get_brand_by_id(id, True))
+    else:
+        return "Error: The request argument 'id' was not provided."
